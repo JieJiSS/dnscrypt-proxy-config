96fd22c830de51826886d198063861055a35eeb2
Ji <c141028@protonmail.com>
2020-02-08 14:52:48 +0800

    Update dnscrypt-proxy-config.sh

--- dnscrypt-proxy-config.sh
+++ dnscrypt-proxy-config.sh
@@ -1,20 +1,20 @@
-wget -N https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf
-echo '# Converted from https://github.com/felixonmars/dnsmasq-china-list/blob/master/accelerated-domains.china.conf' >dnscrypt-forwarding-rules.txt
-echo '# https://github.com/felixonmars/dnsmasq-china-list' >>dnscrypt-forwarding-rules.txt
+wget -N https://github.com/JieJiSS/dnsmasq-china-list/raw/master/accelerated-domains.china.conf
+echo '# Converted from https://github.com/JieJiSS/dnsmasq-china-list/blob/master/accelerated-domains.china.conf' >dnscrypt-forwarding-rules.txt
+echo '# https://github.com/JieJiSS/dnsmasq-china-list' >>dnscrypt-forwarding-rules.txt
 echo '# Thanks to all contributors.' >>dnscrypt-forwarding-rules.txt
 echo '' >>dnscrypt-forwarding-rules.txt
 cat accelerated-domains.china.conf | grep -v '^#server' | sed -e 's|/| |g' -e 's|^server= ||' | sed 's/114.114.114.114/114.114.114.114,223.5.5.5,119.29.29.29/g' >>dnscrypt-forwarding-rules.txt
 
-wget -N https://github.com/felixonmars/dnsmasq-china-list/raw/master/bogus-nxdomain.china.conf
-echo '# Converted from https://github.com/felixonmars/dnsmasq-china-list/blob/master/bogus-nxdomain.china.conf' >dnscrypt-blacklist-ips.txt
-echo '# https://github.com/felixonmars/dnsmasq-china-list' >>dnscrypt-blacklist-ips.txt
+wget -N https://github.com/JieJiSS/dnsmasq-china-list/raw/master/bogus-nxdomain.china.conf
+echo '# Converted from https://github.com/JieJiSS/dnsmasq-china-list/blob/master/bogus-nxdomain.china.conf' >dnscrypt-blacklist-ips.txt
+echo '# https://github.com/JieJiSS/dnsmasq-china-list' >>dnscrypt-blacklist-ips.txt
 echo '# Thanks to all contributors.' >>dnscrypt-blacklist-ips.txt
 echo '' >>dnscrypt-blacklist-ips.txt
 cat bogus-nxdomain.china.conf | grep -v '^#bogus' | grep bogus-nxdomain | sed 's/bogus-nxdomain=//g' >>dnscrypt-blacklist-ips.txt
 
-wget -N https://github.com/missdeer/blocklist/raw/master/toblock-without-shorturl-optimized.lst
-echo '# Converted from https://github.com/missdeer/blocklist/blob/master/toblock-without-shorturl-optimized.lst' >dnscrypt-blacklist-domains.txt
-echo '# https://github.com/missdeer/blocklist' >>dnscrypt-blacklist-domains.txt
+wget -N https://github.com/JieJiSS/blocklist/raw/master/toblock-without-shorturl-optimized.lst
+echo '# Converted from https://github.com/JieJiSS/blocklist/blob/master/toblock-without-shorturl-optimized.lst' >dnscrypt-blacklist-domains.txt
+echo '# https://github.com/JieJiSS/blocklist' >>dnscrypt-blacklist-domains.txt
 echo '# Thanks to all contributors.' >>dnscrypt-blacklist-domains.txt
 echo '' >>dnscrypt-blacklist-domains.txt
 echo 'ad.*' >>dnscrypt-blacklist-domains.txt
